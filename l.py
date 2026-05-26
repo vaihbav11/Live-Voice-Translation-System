bd181b2fa7b6d42e7dfec54e83d2845638dfd59d
Vaibhav Chaturvedi <110451139+vaihbav11@users.noreply.github.com>
2026-05-26 23:48:19 +0530

    Update l.py

--- l.py
+++ l.py
@@ -5,7 +5,7 @@ subprocess.run(["pip", "install", "openai-whisper", "transformers==4.35.0",
 
 import streamlit as st
 import whisper  
-import numpy as np 
+import numpy as np  
 import sounddevice as sd 
 from scipy.io import wavfile 
 import os
